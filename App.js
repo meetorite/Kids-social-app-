a681723d5681ba197e0023cb05b9d47c1a5e5326
meetorite <meet.joshi.3903@gmail.com>
2025-02-07 22:13:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://your-backend-url.onrender.com/users")
+      .then(response => setUsers(response.data))
+      .catch(error => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Kids Social Media</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            <img src={user.profile_picture} alt={user.name} width="50" />
+            {user.name} (Age: {user.age})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;

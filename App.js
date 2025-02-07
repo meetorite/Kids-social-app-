3f59d43dbaecac56d4f68e6b2f6bbcd1948df084
meetorite <meet.joshi.3903@gmail.com>
2025-02-07 23:49:44 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from axios.get("https://your-backend.onrender.com/users")
 
 function App() {
   const [users, setUsers] = useState([]);
